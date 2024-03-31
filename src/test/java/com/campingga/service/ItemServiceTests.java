4c4050b8ddf9462a56d064875814e50250b95850
오주리 <ojl98@naver.com>
2024-03-31 17:12:42 +0900

    ItemService 단위테스트완료, ItemControllerTests 추가

--- src/test/java/com/campingga/service/ItemServiceTests.java
+++ src/test/java/com/campingga/service/ItemServiceTests.java
@@ -1,4 +1,7 @@
-package com.campingga.mapper;
+package com.campingga.service;
+
+
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -6,17 +9,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.campingga.domain.ItemVO;
+
 import lombok.extern.log4j.Log4j;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
-@Log4j 
-public class ItemMapperTests {
+@Log4j
+public class ItemServiceTests {
+	
 	@Autowired
-	private ItemMapper itemMapper;
+	private ItemService itemService;
+	
 	//상품 리스트 출력 테스트
 	@Test
-	public void testGetList() {
-		log.info(itemMapper.selectList());
-	}
+	public void getList() {
+		List<ItemVO> itemList = itemService.getList();
+		itemList.forEach(item -> log.info(item));
+	};
 }
