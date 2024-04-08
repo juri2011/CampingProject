9a01fce3fc5802ceec8dc0f432706cd9ae4c437d
오주리 <ojl98@naver.com>
2024-04-08 20:50:04 +0900

    Spring Security 설정 추가

---
+++ src/main/java/com/campingga/security/domain/CustomUser.java
@@ -0,0 +1,34 @@
+package com.campingga.security.domain;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.zerock.domain.Member;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class CustomUser extends User {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Member member;
+	
+	public CustomUser(String username, String password,
+			Collection<? extends GrantedAuthority> authorities) {
+		super(username, password, authorities);
+	}
+	
+	public CustomUser(Member mem) {
+		super(mem.getMem_id(), mem.getPwd(),
+				mem.getAuthList().stream()
+				.map(auth->new SimpleGrantedAuthority(auth.getAuth()))
+				.collect(Collectors.toList()));
+		this.member = mem;
+	}
+}
